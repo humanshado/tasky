7dc18882bfb8f9ab05dab782ac4683d009f5e6e8
Abubakar Usman <abusmoh@gmail.com>
2018-04-23 13:32:45 -0400

    create .env file

--- src/base.js
+++ src/base.js
@@ -2,12 +2,12 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDqXM7GwjKNmg_pk-T7yyEfzrLhU2Xtgno",
-    authDomain: "tasky-21914.firebaseapp.com",
-    databaseURL: "https://tasky-21914.firebaseio.com",
-    projectId: "tasky-21914",
-    storageBucket: "tasky-21914.appspot.com",
-    messagingSenderId: "305845771535"
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 
 const app = firebase.initializeApp(config);
