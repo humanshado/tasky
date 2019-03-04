f6458ae212ccd651ee37fd048e0e79bcbec0f24f
Abubakar Usman <abusmoh@gmail.com>
2019-03-04 18:16:40 -0500

    fixes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 require('firebase/firestore');
 
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+    apiKey: "AIzaSyDqXM7GwjKNmg_pk-T7yyEfzrLhU2Xtgno",
+    authDomain: "tasky-21914.firebaseapp.com",
+    databaseURL: "https://tasky-21914.firebaseio.com",
+    projectId: "tasky-21914",
+    storageBucket: "tasky-21914.appspot.com",
+    messagingSenderId: "305845771535"
 };
 
 firebase.initializeApp(config);
@@ -17,7 +17,7 @@ db.settings({ timestampsInSnapshots: true });
 
 const auth = firebase.auth();
 
-export { 
+export {
     db,
     auth,
 };
